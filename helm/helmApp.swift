662d5a3dd96bf0e103a4a98b3504a3fade22946e
Kwaku Amo-Korankye <korankye2004@gmail.com>
2026-06-27 10:46:29 +0000

    Initial Commit

---
+++ helm/helmApp.swift
@@ -0,0 +1,17 @@
+//
+//  helmApp.swift
+//  helm
+//
+//  Created by Kwaku Amo-Korankye on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct helmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
